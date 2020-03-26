cb12500fba5152bf7ce232e402f32a458c8ec347
Dhanshri Pawar <dhanshri.pawar@DhanshruP-mini.local>
2020-03-26 18:14:43 +0530

    table view code

---
+++ tableview/tableViewControllerExample/tableViewControllerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tableViewControllerExample
+//
+//  Created by Dhanshri Pawar on 24/03/20.
+//  Copyright © 2020 Dhanshri Pawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
